f7f592c3384e863272a2174cf1c1e40eff0871f4
Lucas Delvoy <lucasdevlvoy@gmail.com>
2026-02-03 16:15:39 +0900

    Model loaded in API

---
+++ backend/app.py
@@ -0,0 +1,49 @@
+import joblib
+from torch import nn, optim, FloatTensor, load
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Model(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.layer1 = nn.Linear(10000, 128)
+        self.first_filter = nn.ReLU()
+        self.hidden_layer = nn.Linear(128, 16)
+        self.second_filter = nn.ReLU()
+        self.layer2 = nn.Linear(16, 1)
+        self.last_filter = nn.Sigmoid()
+    
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.first_filter(x)
+        x = self.hidden_layer(x)
+        x = self.second_filter(x)
+        x = self.layer2(x)
+        x = self.last_filter(x)
+        return x
+
+try:
+    model = Model()
+    model.load_state_dict(load("../output/model.pth", weights_only=True))
+    model.eval()
+    print("Model successfully loaded!")
+except FileNotFoundError:
+    print("Couldn't find model")
+    model = None
+except Exception as e:
+    print(f"Error loading model: {e}")
+    
+vectorizer = joblib.load("../output/vectorizer.pkl")
+
+@app.get("/")
+async def read_root():
+    return{"Hello": "World"}
+
+@app.get("/health")
+async def read_health():
+    if model is not None:
+        return("Model Loaded")
+    else:
+        return("Error, model not loaded")
