a18242b38b9a14c6e1d1d573f64bf71f6baed166
Lucas Delvoy <lucasdevlvoy@gmail.com>
2026-02-14 20:17:52 +0900

    Clean UI

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect} from 'react'
+import './styles.css'
 
 function Index () {
 
@@ -24,7 +25,9 @@ function Index () {
 
             if (mailForm.ok) {
                 const data = await mailForm.json()
+                console.log(data)
                 setMailResult(data)
+                console.log(mailResult)
             } else {
                 setMailResult(null)
                 const errorData = await mailForm.json()
@@ -36,28 +39,49 @@ function Index () {
             setLoading(false)
         }
 
-
     }
+
+    const statusClass = mailResult?.status === 'spam'
+        ? 'bg-spam'
+        : mailResult?.status === 'mail'
+        ? 'bg-mail'
+        : 'bg-default'
 
     return (
         
         <>
-            <form onSubmit={handleSubmit}>
-                <textarea value={emailContent} onChange={(e) => setEmailContent(e.target.value)}></textarea>
-                <button type='submit' disabled={loading || !emailContent || emailContent.length > 5000}>Predict</button>
-            </form>
-            {mailResult ? (
-                <ul>
-                    <li>Status: {mailResult.status}</li>
-                    <li>Score: {mailResult.score}</li>
-                    <li>Conclusion: {mailResult.prediction}</li>
-                </ul>
-            ) : (
-                <p>Please copy a mail into the text area. Mail must be under 5000 characters</p>
-            )}
-            {error ? (
-                <p>{error}</p>
-            ) : null }
+            
+            <div className={`container ${statusClass}`}>
+                <h1 className='main-title'>Spam Detecor</h1>
+                {mailResult ? (
+                    <div className='result-container'>
+                        <div>
+                            <h1>This is a {mailResult.status}</h1>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>Status: {mailResult.status}</li>
+                                <li>Score: {mailResult.score}</li>
+                                <li>Conclusion: {mailResult.prediction}</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <button onClick={() => setMailResult(null)}>New prediction</button>
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <form className='form-container' onSubmit={handleSubmit}>
+                            <p>Please copy a mail into the text area. Mail must be under 5000 characters</p>
+                            <textarea className='textarea' value={emailContent} onChange={(e) => setEmailContent(e.target.value)}></textarea>
+                            <button type='submit' disabled={loading || !emailContent || emailContent.length > 5000}>Predict</button>
+                        </form>
+                    </div>
+                )}
+                {error ? (
+                    <p>{error}</p>
+                ) : null }
+            </div>  
         </>
     )
 }
